bfb529549de8add8963408e95fd1c308cddb28df
Nelson <nelson.vico@gmail.com>
2023-11-24 16:04:07 +0100

    torbes

---
+++ README.md
@@ -0,0 +1,46 @@
+# Forbes Contact Page Clone
+
+Welcome to the Forbes Contact Page Clone Project! This application was developed using HTML, CSS, and JavaScript to faithfully reproduce the Forbes contact page. Here's a quick guide to help you understand and use clone.
+
+![Clone de la Page de Contact Forbes](/public/image-torbes.png)
+
+## Live Demo
+
+Check out the live demo of the application hosted on Vercel: [Forbes Contact Page Clone](https://contact-clone-forbes.vercel.app/)
+
+## Overview
+
+The project aims to replicate the Forbes contact page, maintaining its visual design and functionality. Below are instructions for accessing and running our clone on your own machine.
+
+## How to Run the Project
+
+1. Clone this repository to your computer using the following command :
+    ```bash
+    git clone https://github.com/Snaper014/contact-clone-forbes.git
+
+2. Navigate to the project directory :
+    ```bash
+    cd contact-clone-forbes
+
+3. Open the index.html file in your favorite browser to view the contact page.
+
+## Main Features
+
+1. Faithful Reproduction of the Forbes Contact Page:
+
+The project faithfully reproduces the Forbes contact page, including layout, colors, images and typography.
+
+2. Responsive
+
+Our app is fully responsive, which means it adapts to different screen sizes, from desktops to smartphones.
+
+3. Contact form
+
+An interactive contact form where users can enter their name, email address, phone number and message. Certain warning messages are displayed when the user does not respect certain conditions.
+If certain mandatory fields are not completed or do not correspond to
+conditions, user information is not sent.
+
+
+
+
+
---
+++ build/js/index.js
@@ -0,0 +1,200 @@
+"use strict";
+let ButtonSubmit = document.getElementById("button-submit");
+let MobileButtonSubmit = document.getElementById("button-submit-mobile");
+let firstname = document.getElementById("firstname");
+let email = document.getElementById("email");
+let City = document.getElementById("City");
+let Subject = document.getElementById("Subject");
+let Spam = document.getElementById("label-spam");
+let Name = document.getElementById("Name");
+let Company = document.getElementById("Company");
+let message = document.getElementById("message-input");
+let NumberPhone = document.getElementById("NumberPhone");
+let checkbox = document.getElementById("checkbox");
+let spanFirstName = document.getElementById("warning-firstname");
+let spanEmail = document.getElementById("warning-mail");
+let spanCity = document.getElementById("warning-city");
+let spanSubject = document.getElementById("warning-subject");
+let spanSpam = document.getElementById("warning-spam");
+let SpanCompany = document.getElementById("warning-company");
+let spanName = document.getElementById("warning-name");
+let spanPhone = document.getElementById("warning-phone-number");
+let spanMessage = document.getElementById("warning-text-area");
+let spanChecked = document.getElementById("warning-checked");
+const letters = /[0-9]/g;
+const CheckMail = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/g;
+const CheckPhone = /[0-9]{10}/g;
+const CheckMessage = /[0-9a-zA-Z]{1,400}/g;
+function MessageSpan(span) {
+    span.textContent = "Ce Champs est nécessaire";
+    span.style.fontWeight = "600";
+    span.style.color = "#990000";
+    span.style.marginTop = "5px";
+    span.style.marginBottom = "15px";
+}
+function EmptySpan(span) {
+    span.textContent = "";
+    span.style.marginBottom = "0px";
+}
+function WarningSpan(span, message) {
+    span.textContent = `${message}`;
+    span.style.fontWeight = "600";
+    span.style.color = "#990000";
+    span.style.marginBottom = "15px";
+}
+const Tab = [
+    { input: firstname, span: spanFirstName },
+    { input: email, span: spanEmail },
+    { input: City, span: spanCity },
+    { input: Subject, span: spanSubject },
+    { input: Name, span: spanName },
+    { input: Company, span: SpanCompany },
+    { input: NumberPhone, span: spanPhone },
+    { input: message, span: spanMessage },
+];
+firstname.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    console.log("Value:", value);
+    if (!value.match(letters)) {
+        EmptySpan(spanFirstName);
+    }
+    else {
+        WarningSpan(spanFirstName, "Le champs que vous avez renseignez n'est pas valide");
+    }
+});
+Name.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (!value.match(letters)) {
+        EmptySpan(spanName);
+    }
+    else {
+        WarningSpan(spanName, "Le champs que vous avez renseignez n'est pas valide");
+    }
+});
+email.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (value.match(CheckMail)) {
+        EmptySpan(spanEmail);
+    }
+    else {
+        WarningSpan(spanEmail, "L\'email n\'est pas valide");
+    }
+});
+City.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (!value.match(letters)) {
+        EmptySpan(spanCity);
+    }
+    else {
+        WarningSpan(spanCity, "Le champs que vous avez renseignez n'est pas valide");
+    }
+});
+NumberPhone.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (value.match(CheckPhone)) {
+        EmptySpan(spanPhone);
+    }
+    else {
+        WarningSpan(spanPhone, "Le numéro de téléphone indiquer n\'est pas valide");
+    }
+});
+message.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (value.match(CheckMessage)) {
+        EmptySpan(spanMessage);
+    }
+    if (value.length >= 399) {
+        WarningSpan(spanMessage, "La zone de message est limité à 400 caractères.");
+    }
+});
+Company.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (value !== "") {
+        EmptySpan(SpanCompany);
+    }
+});
+Subject.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (value !== "") {
+        EmptySpan(spanSubject);
+    }
+});
+Spam.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.value;
+    if (value !== "") {
+        EmptySpan(spanSpam);
+    }
+});
+checkbox.addEventListener("input", (e) => {
+    const target = e.target;
+    const value = target.checked;
+    if (value === false) {
+        MessageSpan(spanChecked);
+    }
+    else {
+        EmptySpan(spanChecked);
+    }
+});
+ButtonSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    const Checked = checkbox.checked === false;
+    const ValueSpam = Spam.value !== "2";
+    if (ValueSpam) {
+        WarningSpan(spanSpam, "La valeur de vérification du spam est incorrect");
+    }
+    if (Checked) {
+        MessageSpan(spanChecked);
+    }
+    Tab.forEach((element) => {
+        if (element.input.value === "") {
+            MessageSpan(element.span);
+        }
+    });
+    const ValidationInput = Tab.every(items => items.input.value !== "");
+    const ValidationSpan = Tab.every(items => items.span.textContent === "");
+    console.log("verifications", ValidationInput, ValidationSpan);
+    if (ValidationInput && ValidationSpan && !Checked && !ValueSpam) {
+        Tab.forEach(items => {
+            items.input.value = "";
+        });
+        Spam.value = "";
+        checkbox.checked = false;
+        alert("Vos informations ont bien été envoyées");
+    }
+});
+MobileButtonSubmit.addEventListener("click", (event) => {
+    event.preventDefault();
+    const Checked = checkbox.checked === false;
+    const ValueSpam = Spam.value !== "1";
+    if (ValueSpam) {
+        WarningSpan(spanSpam, "La valeur de vérification du spam est incorrect");
+    }
+    if (Checked) {
+        MessageSpan(spanChecked);
+    }
+    Tab.forEach((element) => {
+        if (element.input.value === "") {
+            MessageSpan(element.span);
+        }
+    });
+    const ValidationInput = Tab.every(items => items.input.value !== "");
+    const ValidationSpan = Tab.every(items => items.span.textContent === "");
+    console.log("verifications", ValidationInput, ValidationSpan);
+    if (ValidationInput && ValidationSpan && !Checked && !ValueSpam) {
+        Tab.forEach(items => {
+            items.input.value = "";
+        });
+        Spam.value = "";
+        checkbox.checked = false;
+        alert("Vos informations ont bien été envoyées");
+    }
+});
